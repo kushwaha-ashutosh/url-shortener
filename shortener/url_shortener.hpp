23661b3c62ffaa059aeed830c750ea0d8c10f881
Ashutosh Kushwaha <ashutoshk2601@gmail.com>
2026-03-10 22:59:11 +0530

    shortener

---
+++ shortener/url_shortener.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+
+class URLShortener{
+    private:
+        std::unordered_map<int, std::string> hashMap;
+        const size_t PRIME = 1000003;
+    public:
+        size_t hashURL(const std::string &url);
+        size_t shortenURL(const std::string &url);
+        std::string getURL(size_t value); 
+};
